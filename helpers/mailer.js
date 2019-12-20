3c65e26bef8562b5761a3194220426b7f8cc3139
fadhilahrayafi <fadhilahrayafi@gmail.com>
2019-12-20 08:35:08 +0700

    update

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -3,19 +3,19 @@ const nodemailer = require("nodemailer");
 const priceEdit = require("./priceEdit")
 
 function mailer(email, productsData, totalPrice) {
-  let products = ''
-  if(!productsData) {
+  let products = 'Dear our lovely custummer, Thankyou for using our service!<br> here is your detail about your purchased product from Skin Type:<br><br>'
+  if (!productsData) {
     products += priceEdit(totalPrice)
   }
   else {
-    for(let i = 0; i < productsData.length; i++){
+    for (let i = 0; i < productsData.length; i++) {
       let newPrice = priceEdit(productsData[i].price)
       products += `Name Product: ${productsData[i].name} | Price: ${newPrice}<br><br>`
     }
-    products += `Total price: ${priceEdit(totalPrice)}`
+    products += `Total price: ${priceEdit(totalPrice)}<br><br>For payment method, we receive from:<br>BCA: 12345678<br>Mandiri: 87654321`
   }
-  
-    // create reusable transporter object using the default SMTP transport
+
+  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -24,7 +24,7 @@ function mailer(email, productsData, totalPrice) {
     }
   });
 
-    // send mail with defined transport object
+  // send mail with defined transport object
   let mailOptions = {
     from: 'skincare.pairproject@gmail.com', // sender address
     to: email, // list of receivers
@@ -33,8 +33,8 @@ function mailer(email, productsData, totalPrice) {
     html: products
   }
 
-  transporter.sendMail(mailOptions, function(err, data) {
-    if(err){
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
       console.log(err);
     }
     else {
